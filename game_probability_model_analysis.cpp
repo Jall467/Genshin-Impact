c0de249f749dce2a4b591a2506b209914a3958aa
Jordan Joe <dqh255757368@outlook.com>
2025-12-21 17:56:05 +0800

    Update game_probability_model_analysis.cpp

--- game_probability_model_analysis.cpp
+++ game_probability_model_analysis.cpp
@@ -77,7 +77,7 @@ void game_select(int i, int& charnum, double& charfiveup, int& gearnum, double& 
 		Wuthering_Waves(char_pos_arr, gear_pos_arr);
 		break;
 	default:
-		printf("输入的GAME值不合法！\n");
+		printf("illegal GAME value!\n");
 		break;
 	}
 }
